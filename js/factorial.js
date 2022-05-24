46d32abe2e1953b6f7a15c88e640b1a39089f7f2
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-05-24 15:42:07 -0400

    added JS file

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,15 @@
 exports.factorial = function(num) {
+    // const factorial = function(num) {
+    
+    if (num===0) {
+        return 1
+    }    
+
+    let answer=1
+    for (let i=num;i>=1;i--) {
+        answer*=i
+    }
+    return answer
 };
+
+// console.log(factorial(45))
